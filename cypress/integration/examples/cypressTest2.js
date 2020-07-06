4dc911b8ab83fefa01243a19e0db2619d6de846e
Doug Mason <bikeracer4487@gmail.com>
2020-07-05 18:12:27 -0600

    Revert to previous commit

--- cypress/integration/examples/cypressTest2.js
+++ cypress/integration/examples/cypressTest2.js
@@ -1,22 +1,18 @@
-import allureReporter from '@wdio/allure-reporter'
-
 describe('My Second Test', () => {
     it('Gets, types and asserts', () => {
         //allureReporter.addFeature('Home Page')
         cy.visit('https://example.cypress.io')
 
-        //allureReporter.addStep('Contains "type" text')
+        cy.pause()
+
         cy.contains('type')
 
-        //allureReporter.addStep('Click on "type"')
         cy.contains('type').click()
         
         // Should be on a new URL which includes '/commands/actions'
-        //allureReporter.addStep('Correct URL')
         cy.url().should('include', '/commands/actions')
 
         // Get an input, type into it and verify that the value has been updated
-        //allureReporter.addStep('Email updated properly')
         cy.get('.action-email')
           .type('fake@email.com')
           .should('have.value', 'fake@email.com')
